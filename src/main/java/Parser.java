a635879fd49779bf4da418c22c9107c625e51edd
Zueff Daniil <70032504+ZueffC@users.noreply.github.com>
2021-08-17 23:13:40 +0300

    Update Parser.java

--- src/main/java/Parser.java
+++ src/main/java/Parser.java
@@ -1,5 +1,6 @@
 import  org.jsoup.*;
 import org.jsoup.nodes.*;
+import org.jsoup.select.Elements;
 
 import java.io.IOException;
 import java.lang.*;
@@ -7,19 +8,19 @@ import java.lang.*;
 public class Parser {
     public static String get_worldweather(String url) throws IOException {
         Document document = Jsoup.connect(url).get();
-        var elems = document.select(".current");
+        Elements elems = document.select(".current");
         return (elems).toString();
     }
 
     public static String get_gismetio(String url) throws IOException {
         Document document = Jsoup.connect(url).get();
-        var elems = document.select("div.tab.tooltip");
+        Elements elems = document.select("div.tab.tooltip");
         return (elems).toString();
     }
 
     public static String get_yandex(String url) throws IOException {
         Document document = Jsoup.connect(url).get();
-        var elems = document.select("div.climate-calendar-day_current_day");
+        Elements elems = document.select("div.climate-calendar-day_current_day");
         return (elems).toString();
     }
     public static void main(String[] args){
